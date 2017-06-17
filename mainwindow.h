be568731e9676a30eee3979eb1b70f4fddf5ae54
alvinlyb <it8c@163.com>
2017-06-17 18:18:42 +0800

    ”QtWidgetAll

---
+++ mainwindow.h
@@ -0,0 +1,25 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QHash>
+#include <QMainWindow>
+#include <QDockWidget>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QHash<QString,QDockWidget *> hashDockWidget;
+    QDockWidget *CalssDockWidget;
+    //QDockWidget *TableItem_DockWidget;
+};
+
+#endif // MAINWINDOW_H
